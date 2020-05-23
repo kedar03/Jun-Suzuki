507ce3f4c5f9aea81cc95992ac50b4da40f1ae30
kedar03 <38580837+kedar03@users.noreply.github.com>
2020-05-22 22:50:46 -0400

    Add files via upload

---
+++ HW9/src/edu/umb/cs680/hw09/APFS.java
@@ -0,0 +1,27 @@
+package edu.umb.cs680.hw09;
+
+import java.time.LocalDateTime;
+
+public class APFS extends FileSystem{
+	public static APFS getAPFSFileSystem() {
+		if(classinst==null)
+			classinst = new APFS ();
+		return classinst;
+	}
+	private static APFS classinst = null;
+	@Override
+	protected FSElement createDefaultRoot() {
+		LocalDateTime localTime = LocalDateTime.of(2020, 05, 12, 0, 0);
+		ApfsDirectory root = new ApfsDirectory(null, "root", 0, localTime, "kedar", localTime);
+		return root;
+	}
+	private APFS() {};
+	
+	
+
+	
+	
+	public static void main(String[] args) {
+		System.out.println("Success from APFS");
+	}
+}
